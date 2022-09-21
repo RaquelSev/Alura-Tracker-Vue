b6a9cd0f2b2188f4bc2c70d9476dda420b9f1dd0
Raquel Sev <raquel.sev.celia@gmail.com>
2022-09-21 16:17:34 -0300

    Trocar tema claro/escuro
    Colocar tema claro e escuro e fazer com que mude a partir do botão

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -3,6 +3,7 @@
     <h1>
       <img src="../assets/logo.png" alt="" />
     </h1>
+    <button class="button" @click="alterarTema">{{ textoBotao }}</button>
   </header>
 </template>
 
@@ -11,6 +12,27 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "BarraLateral",
+  emits: ["aoTemaAlterado"],
+  data() {
+    return {
+      modoEscuroAtivo: true,
+    };
+  },
+  computed: {
+    textoBotao() {
+      if (this.modoEscuroAtivo) {
+        return "Desativar modo Escuro";
+      } else {
+        return "Ativar modo Escuro";
+      }
+    },
+  },
+  methods: {
+    alterarTema() {
+      this.modoEscuroAtivo = !this.modoEscuroAtivo;
+      this.$emit("aoTemaAlterado", this.modoEscuroAtivo);
+    },
+  },
 });
 </script>
 
@@ -20,6 +42,7 @@ header {
   background-color: #0d3b66;
   width: 100%;
   height: 100vh;
+  text-align: center;
 }
 @media only screen and (max-width: 768px) {
   header {
